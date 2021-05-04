b2cf62f79ac57f9e7654eabbd9d7bdfa45eafbd5
mailf <mailf.dev@yandex.ru>
2021-05-04 22:44:40 +0300

    Unlimited page size of arts and return only "show" enabled

--- app/arts/views.py
+++ app/arts/views.py
@@ -7,6 +7,7 @@ from app.arts.serializers import ArtWorkSerializer
 class ArtWorkViewSet(viewsets.ReadOnlyModelViewSet):
     permission_classes = []
     serializer_class = ArtWorkSerializer
-    queryset = ArtWork.objects.all()
+    queryset = ArtWork.objects.filter(show=True)
     filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
     filterset_fields = 'type',
+    pagination_class = None
